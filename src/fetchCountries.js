97cc0483943cfed5b32b6ab73a37f4c561ca400e
Maciej Krogulski <maciejkrogulski1@gmail.com>
2023-01-23 18:14:45 +0100

    update- almost done

---
+++ src/fetchCountries.js
@@ -0,0 +1,58 @@
+
+
+function fetchCountries(name) {
+    fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then (response => response.json())
+    .then (data => {
+        const mappedData = data.map(item => {
+            return {
+                name: item.name.official,
+                capital: item.capital,
+                population: item.population,
+                flag: item.flags.svg,
+                languages: item.languages,
+            }
+        });
+        console.log(mappedData)
+
+        const list = document.querySelector('.country-list')
+        const info = document.querySelector('.country-info')
+
+        if (mappedData.length > 10) {
+          //  window.alert('To many matches')
+          list.innerHTML = '' 
+          info.innerHTML = ''
+        }
+        if (mappedData.length >= 2 && mappedData.length <= 10) {
+            list.innerHTML = '' 
+            info.innerHTML = ''
+            for (let i = 0; i < mappedData.length; i++) {
+
+                const element = mappedData[i];
+                const country = document.createElement("li")
+                country.classList.add("country-item")
+                country.innerHTML =`<img class="country-flag"  src=${element.flag}> ${element.name}`
+                list.append(country)
+            }
+        }
+        if (mappedData.length === 1) {
+            list.innerHTML = '' 
+            const countryInfo = mappedData[0]
+            console.log(countryInfo)
+            info.innerHTML = `<ul class="country-info-list">
+            <li><img class="country-flag-info"  src=${countryInfo.flag}> <span class="country-info-name">${countryInfo.name}</span></li>
+            <li><strong>Capital:</strong> ${countryInfo.capital}</li>
+            <li><strong>Population:</strong> ${countryInfo.population}</li>
+            <li><strong>Languages:</strong> ${countryInfo.languages}</li>
+          </ul>`
+
+            
+        }
+    })
+    .catch (error => {
+        console.log('errors')
+    })
+}
+
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('input')
+//const list = document.querySelector('.country-list')
+
+
+input.addEventListener('input', (event) => {
+
+    const name = event.currentTarget.value
+    console.log(name)
+    fetchCountries(name)
+
+})
