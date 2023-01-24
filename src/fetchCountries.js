1b89c6ea377d36f376bfa9d8fc7bf0b281d97964
Maciej Krogulski <maciejkrogulski1@gmail.com>
2023-01-24 16:49:14 +0100

    update data

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -7,31 +7,30 @@ function fetchCountries(name) {
   fetch(`https://restcountries.com/v3.1/name/${name}?fields=languages,name,official,population,flags`)
     .then(response => response.json())
     .then(data => {
-      const mappedData = data
-        console.log(mappedData)
 
-      if (mappedData.length > 10) {
+
+      if (data.length > 10) {
         Notiflix.Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
         list.innerHTML = '';
         info.innerHTML = '';
       }
-      if (mappedData.length >= 2 && mappedData.length <= 10) {
+      if (data.length >= 2 && data.length <= 10) {
         list.innerHTML = '';
         info.innerHTML = '';
-        for (let i = 0; i < mappedData.length; i++) {
+        for (let i = 0; i < data.length; i++) {
         
-          const element = mappedData[i];
+          const element = data[i];
           const country = document.createElement('li');
           country.classList.add('country-item');
           country.innerHTML = `<img class="country-flag"  src=${element.flags.svg}> ${element.name.official}`;
           list.append(country);
         }
       }
-      if (mappedData.length === 1) {
+      if (data.length === 1) {
         list.innerHTML = '';
-        const countryInfo = mappedData[0];
+        const countryInfo = data[0];
         const languages = Object.values(countryInfo.languages)
 
         info.innerHTML = `<ul class="country-info-list">
